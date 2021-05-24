9f241cce6751255348242e8c998a717f2cdd70a7
Simon Weniger <hello@simonweniger.com>
2021-05-24 11:08:32 +0200

    added divider, drawer, dropdown, navigation

--- docs/src/App.js
+++ docs/src/App.js
@@ -15,15 +15,19 @@ import BreadcrumbsList from "./pages/BreadcrumbsList";
 import ButtonList from "./pages/ButtonList";
 import ButtonGroupList from "./pages/ButtonGroupList";
 import CardList from "./pages/CardList";
+import DrawerList from "./pages/DrawerList";
+import NavList from "./pages/NavList";
+import DropdownList from "./pages/DropdownList";
 
 function App() {
   return (
     <Router>
-      <main className="bg-gray-100 dark:bg-gray-800 rounded-2xl relative h-screen overflow-hidden">
+      <main className="bg-white relative h-screen overflow-hidden">
         <div className="flex items-start justify-between">
           <Sidebar />
           <div className="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
             <Header />
+            <div className="mx-4 md:mx-0 bg-gray-100 shadow-inner dark:bg-gray-800 rounded-2xl p-10">
               <Switch>
                 <Route path="/accordion" component={AccordionList} />
                 <Route path="/alert" component={AlertList} />
@@ -33,7 +37,12 @@ function App() {
                 <Route path="/button" component={ButtonList} />
                 <Route path="/button-group" component={ButtonGroupList} />
                 <Route path="/card" component={CardList} />
+                <Route path="/divider" component={CardList} />
+                <Route path="/drawer" component={DrawerList} />
+                <Route path="/navigation" component={NavList} />
+                <Route path="/dropdown" component={DropdownList} />
               </Switch>
+            </div>
           </div>
         </div>
       </main>
--- docs/src/components/Header.js
+++ docs/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "../logo.svg";
 
 function Header() {
   return (
-    <header className="w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
-    <div className="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
+    <header className="w-full bg-white dark:bg-gray-700 flex h-16 rounded-2xl z-40">
+    <img src={logo} className="lg:hidden ml-4 mt-2 h-12" alt="MintFlow UI" />
+    <div className="w-full relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
         <div className="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
             <div className="container relative left-0 z-50 flex w-3/4 h-full">
                 <div className="relative flex items-center w-full lg:w-64 h-full group">
@@ -23,7 +25,7 @@ function Header() {
                         </div>
                     </div>
                 </div>
-                <div className="relative p-1 flex items-center justify-end w-1/4 ml-5 mr-4 sm:mr-0 sm:right-auto">
+                <div className="lg:hidden relative p-1 flex items-center justify-end w-1/4 ml-5 mr-4 sm:mr-0 sm:right-auto">
                     <a href="#" className="block relative">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
